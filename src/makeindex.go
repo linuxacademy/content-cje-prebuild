3b92cb44957bb9b34a55cdda0dc8c57678337f7f
darealmc <michael.mcclaren@linuxacademy.com>
2018-10-31 15:34:30 -0500

    added parameter

--- src/makeindex.go
+++ src/makeindex.go
@@ -12,9 +12,10 @@ func main() {
 		log.Fatal("Cannot create file", err)
 	}
 	defer file.Close()
+	v := os.Getenv("name")
 	s := `<html>
 <body>
-<h2>Hello the Tomcat server is running!</h2>
+<h2>Hello`+ v +` the Tomcat server is running!</h2>
 <h1>The curent server time is <%= new java.util.Date() %></h1>
 </body>
 </html>`
